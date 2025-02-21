5994e0fdf8d9ce98c4bdec012fb66e6058c283be
Romulo Sousa <romulo22.dev@gmail.com>
2025-02-21 20:49:18 -0300

    alterações para tentar vizualizar o módulo react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+// import 'react-vertical-timeline-component/style.min.css';
+
 
 const App = () => {
   return (
